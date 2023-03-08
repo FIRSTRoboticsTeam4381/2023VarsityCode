d97a442708afc0fd6bae0f7e07a806e5a4ff9fa3
Ryan <rzarnold05@outlook.com>
2023-03-07 19:10:43 -0500

    Added DataLogger

--- 2023VarsityCode/src/main/java/frc/robot/Robot.java
+++ 2023VarsityCode/src/main/java/frc/robot/Robot.java
@@ -4,7 +4,10 @@
 
 package frc.robot;
 
+
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -31,6 +34,9 @@ public class Robot extends TimedRobot {
     ctreConfigs = new CTREConfigs();
    
     CameraServer.startAutomaticCapture();
+
+    DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog());
 
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
